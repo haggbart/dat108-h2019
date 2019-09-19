37dfca378a24b1561a5a23ced16eada83e4af84e
Lars-Petter Helland <lars-petter.helland@hvl.no>
2019-09-19 10:10:29 +0200

    F10c til timen

---
+++ F10c - PRG Resp-koder og -headere/src/no/hvl/dat108/TaImotServlet.java
@@ -0,0 +1,24 @@
+package no.hvl.dat108;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/taimot")
+public class TaImotServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		//Målet med hele greien er å skrive ut på en side den hemmelige meldingen
+		//som er "skjult" i en hidden parameter.
+		
+		//Repeter PRG. Deretter sjekk med F12 hva som skjer!!!
+	
+	}
+
+}
